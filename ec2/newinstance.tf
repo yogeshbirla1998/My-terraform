debf480a0b3c3c70b3c2a60c7e3de66a8a36c23e
Ubuntu <ubuntu@ip-172-31-44-135.ec2.internal>
2023-08-30 09:45:56 +0000

    jump-box added

---
+++ ec2/newinstance.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+  config = {
+    path = "../vpc/terraform.tfstate"
+  }
+}
+
+#private instance creation
+
+resource "aws_instance" "priv_instance" {
+  count = var.instance_count
+
+  root_block_device {
+    volume_size = var.storage_size
+  }
+
+  ami                         = var.ami-Id
+  instance_type               = var.InstanceType
+  associate_public_ip_address = false
+
+  vpc_security_group_ids = [aws_security_group.security_sg.id]
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.private_subnets, count.index)
+
+  key_name  = var.key_name
+  user_data = file("./myscript.sh")
+
+  tags = {
+    Name   = "instance-${count.index + 1}"
+    Owner  = var.Owner
+    mailID = var.mailID
+  }
+}
+
+
+#Public Instance for Jump-Box Creation
+
+resource "aws_instance" "public_instance" {
+  root_block_device {
+    volume_size = var.storage_size
+  }
+
+  ami                         = var.ami-Id
+  instance_type               = var.InstanceType
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.security_sg_pub.id]
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+
+  key_name  = var.key_name
+  user_data = file("./myscript.sh")
+
+  tags = {
+    Name   = "instance-jump-box"
+    Owner  = var.Owner
+    mailID = var.mailID
+  }
+}
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -9,4 +9,5 @@ variable "storage_size" {}
 variable "key_name" {}
 variable "ami-Id" {}
 variable "instance_count" {}
-variable "security_sg_name" {}
+variable "security_sg_pri_name" {}
+variable "security_sg_pub_name" {}
