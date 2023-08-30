e6f34fde36f690ca8705ea64baeba52eb78a7807
Ubuntu <ubuntu@ip-172-31-44-135.ec2.internal>
2023-08-30 07:15:11 +0000

    modified ec2

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,36 +1,35 @@
 provider "aws" {
-  /* region = var.region */
   region = "us-east-1"
 }
 
 data "terraform_remote_state" "vpc" {
-  backend = "remote"
-
+  backend = "local"
   config = {
-    organization = "platformEngineering"
-    workspaces = {
-      name = "vpc"
-    }
+    path = "../vpc/terraform.tfstate"
   }
 }
 
-resource "aws_instance" "priv-instance" {
+resource "aws_instance" "priv_instance" {
+  count = var.instance_count
+  
   root_block_device {
     volume_size = var.storage_size
   }
+  
   ami                         = var.ami-Id
   instance_type               = var.InstanceType
   associate_public_ip_address = false
-  count                       = var.instance_count
-  /* subnet_id                   = var.subnet_id 
-  vpc_security_group_ids      = [var.security_group_id] */
-  vpc_security_group_ids = ["sg-0f9651f4874813256"]
-  subnet_id              = element(data.terraform_remote_state.vpc.outputs.private_subnet_ids, count.index)
-  key_name               = var.key_name
-  user_data              = file("./myscript.sh")
+  
+  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.default_security_group_id]
+  subnet_id              = element(data.terraform_remote_state.vpc.outputs.private_subnets, count.index)
+  
+  key_name   = var.key_name
+  user_data  = file("./myscript.sh")
+  
   tags = {
     Name   = "instance-${count.index + 1}"
     Owner  = var.Owner
     mailID = var.mailID
   }
 }
+
