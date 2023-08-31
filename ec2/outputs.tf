efd9a4b8e5f97ac666270a0312932bc1561872ad
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-08-31 11:54:35 +0530

    Update outputs.tf

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,7 +1,7 @@
-output "private_ips" {
-
-  description = "Private IP addresses of the EC2 instances"
-
-  value = aws_instance.priv_instance[*].private_ip
-
+output "instance_private_ip" {
+  value = aws_instance.priv-instance.private_ip
+}
+output "public_ips" {
+  description = "Public IP addresses of the Jump-Box instance"
+  value       = aws_instance.public_instance.public_ip
 }
