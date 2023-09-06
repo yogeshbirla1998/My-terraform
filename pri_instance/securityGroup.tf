71db29121159d93c9b94f52e0409f968b4bb75ba
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2023-09-06 15:44:49 +0530

    Update securityGroup.tf

--- pri_instance/securityGroup.tf
+++ pri_instance/securityGroup.tf
@@ -28,23 +28,22 @@
 } */
 
 resource "aws_security_group" "security_sg" {
-    name        = var.security_sg_pri_name
-    description = "Allow HTTPS to web server"  #data.terraform_remote_state.vpc.outputs.default_security_group_id
+    description = "Allow HTTPS to web server"  # Replace with your description
     vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
-    count = 6 
+  
     ingress {
-        description = "kubernetes inbound ports"
-        from_port   = element([6443, 2379, 10250, 10259, 10257, 30000], count.index)
-        to_port     = element([6443, 2380, 10250, 10259, 10257, 32767], count.index)
-        protocol    = "tcp"
-        cidr_blocks = [
-            data.terraform_remote_state.vpc.outputs.vpc_cidr_block
-        ]
+      description = "Kubernetes inbound ports"
+      from_port   = [6443, 2379, 10250, 10259, 10257, 30000]
+      to_port     = [6443, 2380, 10250, 10259, 10257, 32767]
+      protocol    = "tcp"
+      cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
     }
+  
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-   }
-}
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  
