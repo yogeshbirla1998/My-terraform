0d633380a539450e3b593a8c530bfefeb7c3bd59
Ubuntu <ubuntu@ip-172-31-7-154.ap-south-1.compute.internal>
2023-09-08 11:09:20 +0000

    private instance sg updated

--- pri_instance/securityGroup.tf
+++ pri_instance/securityGroup.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "security_sg"  {
 resource "aws_security_group_rule" "inbound_rules" {
     count = 6  
     type        = "ingress"
-    from_port   = element([22,80,6443, 2379, 10250, 10259, 10257, 30000], count.index)
-    to_port     = element([22,80,6443, 2380, 10250, 10259, 10257, 32767], count.index)
+    from_port   = element([22, 80], count.index)
+    to_port     = element([22, 80], count.index)
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.vpc_cidr_block]
     security_group_id = aws_security_group.security_sg.id
