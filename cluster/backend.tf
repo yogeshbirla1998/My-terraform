7c6e2768cf9520a4ce0a45b8d6bb9644a74e9321
yogeshbirla1998 <141988983+yogeshbirla1998@users.noreply.github.com>
2023-09-22 11:29:54 +0530

    Update backend.tf

--- cluster/backend.tf
+++ cluster/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "pe2dev"
-    key    = "platform_Enginnering/cluster/terraform.tfstate"
+    key  = "platform_Enginnering/cluster/xyz1/terraform.tfstate" 
     region = "us-east-1"
   }
 }
